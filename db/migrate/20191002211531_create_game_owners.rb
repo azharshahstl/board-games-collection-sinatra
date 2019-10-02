f67beaf614adae518a15ee73cc82759581b08aa1
Azhar Shah <azharshahstl@yahoo.com>
2019-10-02 21:19:33 +0000

    added 1 migration and changed password

---
+++ db/migrate/20191002211531_create_game_owners.rb
@@ -0,0 +1,8 @@
+class CreateGameOwners < ActiveRecord::Migration
+  def change
+    create_table :game_owners |t| 
+    t.string :username 
+    t.string :email 
+    t.string :password_digest
+  end
+end
---
+++ db/migrate/20191002211619_create_manufacturers.rb
@@ -0,0 +1,4 @@
+class CreateManufacturers < ActiveRecord::Migration
+  def change
+  end
+end
