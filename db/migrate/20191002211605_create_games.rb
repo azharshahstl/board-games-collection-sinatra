680535e95f43ff17eafc2bca7affa4b34e984398
Azhar Shah <azharshahstl@yahoo.com>
2019-10-02 21:27:39 +0000

    added game_info to t.string

--- db/migrate/20191002211605_create_games.rb
+++ db/migrate/20191002211605_create_games.rb
@@ -5,7 +5,7 @@ class CreateGames < ActiveRecord::Migration
       t.string :manufacturer 
       t.integer :number_of_players
       t.string :est_time_to_play 
-      t.string :
+      t.string :game_info
       end
    end
 end
