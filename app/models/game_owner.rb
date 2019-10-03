5e81e6cfad6876baed03650d76f9cb6606934c1c
Azhar Shah <azharshahstl@yahoo.com>
2019-10-03 18:16:52 +0000

    added rake console, reran migrations

--- app/models/game_owner.rb
+++ app/models/game_owner.rb
@@ -1,4 +1,4 @@
-class GameOwner < ActiveRecord::RUBY_RELEASE_DATE 
+class GameOwner < ActiveRecord::Base 
   has_many :games
   validates :username, :email, :password_digest, presence: true
   validates :username, uniqueness: true
