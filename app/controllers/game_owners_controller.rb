7e48a70242ce211a9606fad109e62d4ea9901ff7
Azhar Shah <azharshahstl@yahoo.com>
2019-10-04 19:03:28 +0000

    added helper methods

--- app/controllers/game_owners_controller.rb
+++ app/controllers/game_owners_controller.rb
@@ -1,6 +1,12 @@
 class GameOwnersController < ApplicationController 
 
-
+get '/signup' do 
+    if logged_in? 
+      redirect '/tweets'
+    else 
+      erb :'/users/signup'
+    end
+  end
 
 
 
