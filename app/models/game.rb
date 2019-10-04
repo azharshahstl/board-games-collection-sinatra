1aee21b3f71694d7b426ed6a90b6834cc3a2924c
Azhar Shah <azharshahstl@yahoo.com>
2019-10-04 20:13:06 +0000

    added slug methods to game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,12 @@
 class Game < ActiveRecord::Base 
   belongs_to :manufacturer 
   has_many :game_owners
+  
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    Artist.all.find { |artist| artist.slug == slug }
+  end
 end
