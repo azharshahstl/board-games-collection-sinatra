1a07c43ae798f1a93faf223b1049d1d9f583e191
Azhar Shah <azharshahstl@yahoo.com>
2019-10-07 22:26:49 +0000

    Built out /signup action and render pages

--- app/controllers/game_owners_controller.rb
+++ app/controllers/game_owners_controller.rb
@@ -8,5 +8,16 @@ class GameOwnersController < ApplicationController
     end
   end
   
+  post '/signup' do 
+    @gameowner = GameOwner.new(username: params[:username], email: params[:email], password_digest: params[:password])
+    if @gameowner.save 
+      erb :'games/games' 
+    else 
+      flash[:failure] = "You did not follow my explicit instructions and now shall be publically shamed!  Or you choose a username that already existed, which is against the rules.  Choose another.....choose wisely."
+      redirect 'signup'
+    end 
+  end 
+    
+  
 
 end
